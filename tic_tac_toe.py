cafd688ac3fc142cef77f79c4442a21a623e72f1
Nitin Tiwari <tiwarinitin94@gmail.com>
2022-06-14 10:54:52 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,67 +1,81 @@
+a = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
+count = 0
+
+
 def printTicTocToe():
     for i in a:
         for k in i:
-            print (k,end="")
-        print("")
-        
+            print(str(k)+" ", end="")
+        print("\n")
+
+
 def isEmpty():
     for i in a:
         for k in i:
-            if k==-1:
+            if k == -1:
                 return True
     return False
 
-def isValidCheck(x,y):
+
+def isValidCheck(x, y):
     if (x <= 2 and y <= 2 and a[x][y] == -1):
         return True
     else:
         return False
 
-def checkLeftRight(x,z):
-    for y in range(2):
+
+def checkLeftRight(x, z):
+    for y in range(3):
         if(a[x][y] != z):
-            
+
             return False
 
     return True
 
+
 def checkUpDown(y, z):
-    for x in range(2):
-        if(a[x][y]!=z ):
+    for x in range(3):
+        if(a[x][y] != z):
             return False
 
-    return False
+    return True
 
-def checkDiagonal(x,y,z):
-    if (x==1 and y==1) or (x==0 and y==2) or (x==2 and y==0) or (x==0 and y==0) or (x==2 and y==2):
-        if(a[1][1]==z and a[0][2]==z and a[2][0]==z):
+
+def checkDiagonal(x, y, z):
+    if (x == 1 and y == 1) or (x == 0 and y == 2) or (x == 2 and y == 0) or (x == 0 and y == 0) or (x == 2 and y == 2):
+        if(a[1][1] == z and a[0][2] == z and a[2][0] == z):
             return True
-        elif(a[1][1]==z and a[0][0]==z and a[2][2]==z):
+        elif(a[1][1] == z and a[0][0] == z and a[2][2] == z):
             return True
         else:
             return False
-    
 
-a=[[-1,-1,-1], [-1,-1,-1], [-1,-1,-1]]
-count = 0
+
 while (isEmpty()):
     count += 1
-    x,y = input().split()
+    printTicTocToe()
+    print("Enter Comma seperated coordinants For placing your move")
+
+    x, y = input().split()
+
     x = int(x)
     y = int(y)
-    if (isValidCheck(x,y)):
-        if(count%2 == 0):
+    if (isValidCheck(x, y)):
+        if(count % 2 == 0):
             z = 0
-            a[x][y]= z
+            a[x][y] = z
         else:
             z = 1
-            a[x][y]= z
-        printTicTocToe()
-        if(checkLeftRight(x,z) or checkUpDown(y,z) or checkDaigonal(x,y,z)):
+            a[x][y] = z
+
+        if(checkLeftRight(x, z) or checkUpDown(y, z) or checkDiagonal(x, y, z)):
+            printTicTocToe()
             print("Congratulations u win this game.")
             break
-        
+
     else:
         count -= 1
         print("Invalid co-ordinates!")
 
+else:
+    print("No space left")
